42a147eb8103db23976ffdea624dc6aab06ea078
ArjunGupte1 <agupte1011@gmail.com>
2020-02-08 19:47:23 -0800

    added ID code for PID

--- 6822robotcode2020/src/main/java/frc/robot/commands/TurnHorizontal.java
+++ 6822robotcode2020/src/main/java/frc/robot/commands/TurnHorizontal.java
@@ -13,11 +13,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class TurnHorizontal extends Command {
-    double P = 0.01; // tune this
+    double P = 0.02; // tune this
+    double I = 0.0;
+    double D = 0.0003;
+
     int cameraX = 320;
     int errorX = 0;
     int targetX;
     double voltage = 0;
+
+    double integral, previous_error, error, derivative = 0;
+    double dt = 0.02;
+    double ff = 0.13;
+    double maxVoltage = 0.40 + ff;
 
     public TurnHorizontal(int diffX) {
         requires(Robot.m_drivetrain);
@@ -35,8 +43,15 @@ public class TurnHorizontal extends Command {
     protected void execute() {
         // The condition if error = 0 is being checked in visionController, thats why you dont need it here
         // this turn vertical command is only being called when the condition ^ is false
-        voltage = P * errorX;
-        turret.rotate(voltage);
+        integral += (errorX * dt); // Integral is increased by the error*time (which is .02 seconds using normal IterativeRobot)
+        derivative = (errorX - previous_error) / dt;
+        previous_error = errorX; //keep updating error to the most recently measured one
+        double voltage = (P * errorX + I * this.integral + D * derivative);
+        voltage += (error > 0 ? ff : -ff);
+        if(Math.abs(voltage) >= maxVoltage){
+            voltage = Math.signum(voltage) * maxVoltage;
+            turret.rotate(voltage);
+        }
         SmartDashboard.putString("Am I aligning?", "yes");     
           
     }
